ad3cc1f6f0c2b629e48c602f5f5a09dcd70f81dc
o Silva <leonardoibvz@gmail.com>
2026-05-20 08:26:22 -0300

    Implementando Categoria e Relacionamentos

--- MinhaApiComSQLite/Data/AppDbContext.cs
+++ MinhaApiComSQLite/Data/AppDbContext.cs
@@ -1,14 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MinhaApiComSQLite.Models;
 
 namespace MinhaApiComSQLite.Data
 {
-
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
 
         public DbSet<Produto> Produtos { get; set; }
-    }
+        public DbSet<Categoria> Categorias { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Regra do teste: Nome do produto deve ser único
+            modelBuilder.Entity<Produto>()
+                .HasIndex(p => p.Nome)
+                .IsUnique();
+
+            // Regra do teste: Nome da categoria deve ser único
+            modelBuilder.Entity<Categoria>()
+                .HasIndex(c => c.Nome)
+                .IsUnique();
+        }
+    }
 }
