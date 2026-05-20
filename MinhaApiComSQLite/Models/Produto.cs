c6dceee3ccb3552580c6470c50a0104728db7e41
o Silva <leonardoibvz@gmail.com>
2026-05-20 08:21:30 -0300

    Ajustando tabela Produto
    Ajustadas as propriedades da tabela Produtos, inserindo o ID da categoria.

--- MinhaApiComSQLite/Models/Produto.cs
+++ MinhaApiComSQLite/Models/Produto.cs
@@ -1,10 +1,12 @@
-﻿namespace MinhaApiComSQLite.Models
+namespace MinhaApiComSQLite.Models
 {
     public class Produto
     {
         public int Id { get; set; }
         public string Nome { get; set; } = string.Empty;
-        public string Descricao { get; set; } = string.Empty;
         public decimal Preco { get; set; }
+        
+        public int CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
     }
 }
